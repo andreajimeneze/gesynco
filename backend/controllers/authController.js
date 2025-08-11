0354a527e21a3df53f37b103a885478c90b08581
Andrea Jiménez <andreacjimenez@gmail.com>
2025-08-10 20:10:18 -0400

    login y registro con hash

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,18 +1,67 @@
-import  { UserModel }  from "../models/index.js";
+import { UserModel } from "../models/index.js";
+import bcrypt from "bcryptjs";
 
 export const login = async (req, res) => {
-  
   try {
     const { email, password } = req.body;
-    const user = await UserModel.findOne({ Where: { email } });
-    if (!user || user.email !== email) {
-      return res.status(404).json({ message: "Usuario no existe o incorrecto" });
+
+    const user = await UserModel.findOne({ where: { email } });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Usuario o contraseña incorrectos" });
     }
+
     if (password !== user.contrasenia) {
-      return res.status(400).json({ message: "Contraseña incorrecta" });
+      return res
+        .status(401)
+        .json({ message: "Usuario o contraseña incorrectos" });
     }
-    res.status(200).json({ message: "Usuario logueado correcta" });
+    if (user) {
+      if (user.rol !== "admin") {
+        return res.status(403).json({ message: "Usuario no autorizado" });
+      }
+    }
+    return res.status(200).json({ message: "Usuario logueado correctamente" });
   } catch (error) {
-    res.status(500).json({ error: "No se pudo conectar a la base de datos" });
+    console.error(error);
+    return res.status(500).json({ error: "Error interno del servidor" });
+  }
+};
+
+export const register = async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  const user = await UserModel.findOne({ where: { email } });
+  const rol = "user";
+
+  try {
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "la contraseña no son iguales" });
+    }
+
+    if (user) {
+      return res
+        .status(409)
+        .json({ error: "Usuario ya existe en nuestra base de datos" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userCreated = await UserModel.create({
+      nombre: name,
+      email,
+      contrasenia: hashedPassword,
+      rol,
+    });
+
+    res
+      .status(201)
+      .json({
+        message: `${userCreated.nombre}, su usuario ha sido creado correctamente`,
+      });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error interno del servidor" });
   }
 };
